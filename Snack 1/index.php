aff6929e1968773c5e635d264efe8e119d8c5789
AnTosi <andrea.tosi.uni@gmail.com>
2021-12-02 15:26:58 +0100

    do snack 1

--- Snack 1/index.php
+++ Snack 1/index.php
@@ -5,66 +5,64 @@ Olimpia Milano - Cantù | 55-60 -->
 
 <?php 
 
-$giornata2 = [
-        'partita' => [
-            [ 
-                'squadra_casa' => 'AX Armani Exchange Milano',
-                'squadra_ospite' => 'Virtus Segafredo Bologna',
-                'punteggio_casa' => 55,
-                'punteggio_ospite' => 60
-            ],
+$giornata2 = [ 
+                [ 
+                    'squadra_casa' => 'AX Armani Exchange Milano',
+                    'squadra_ospite' => 'Virtus Segafredo Bologna',
+                    'punteggio_casa' => 55,
+                    'punteggio_ospite' => 60
+                ],
 
-            [ 
-                'squadra_casa' => 'Dolomiti Energia Trentino',
-                'squadra_ospite' => 'Happy Casa Brindisi',
-                'punteggio_casa' => 63,
-                'punteggio_ospite' => 59
-            ],
+                [ 
+                    'squadra_casa' => 'Dolomiti Energia Trentino',
+                    'squadra_ospite' => 'Happy Casa Brindisi',
+                    'punteggio_casa' => 63,
+                    'punteggio_ospite' => 59
+                ],
 
-            [ 
-                'squadra_casa' => 'GeVi Napoli',
-                'squadra_ospite' => 'Umana Reyer Venezia',
-                'punteggio_casa' => 68,
-                'punteggio_ospite' => 76
-            ],
+                [ 
+                    'squadra_casa' => 'GeVi Napoli',
+                    'squadra_ospite' => 'Umana Reyer Venezia',
+                    'punteggio_casa' => 68,
+                    'punteggio_ospite' => 76
+                ],
 
-            [ 
-                'squadra_casa' => 'Carpegna Prosciutto Pesaro',
-                'squadra_ospite' => 'Allianz Pallacanestro Trieste',
-                'punteggio_casa' => 72,
-                'punteggio_ospite' => 95
-            ],
+                [ 
+                    'squadra_casa' => 'Carpegna Prosciutto Pesaro',
+                    'squadra_ospite' => 'Allianz Pallacanestro Trieste',
+                    'punteggio_casa' => 72,
+                    'punteggio_ospite' => 95
+                ],
 
-            [ 
-                'squadra_casa' => 'Nutribullet Treviso Basket',
-                'squadra_ospite' => 'Unahotels Reggio Emilia',
-                'punteggio_casa' => 62,
-                'punteggio_ospite' => 67
-            ],
+                [ 
+                    'squadra_casa' => 'Nutribullet Treviso Basket',
+                    'squadra_ospite' => 'Unahotels Reggio Emilia',
+                    'punteggio_casa' => 62,
+                    'punteggio_ospite' => 67
+                ],
 
-            [ 
-                'squadra_casa' => 'Bertram Derthona Tortona',
-                'squadra_ospite' => 'Vanoli Basket Cremona',
-                'punteggio_casa' => 65,
-                'punteggio_ospite' => 60
-            ],
+                [ 
+                    'squadra_casa' => 'Bertram Derthona Tortona',
+                    'squadra_ospite' => 'Vanoli Basket Cremona',
+                    'punteggio_casa' => 65,
+                    'punteggio_ospite' => 60
+                ],
 
-            [ 
-                'squadra_casa' => 'Germani Brescia',
-                'squadra_ospite' => 'Openjobmetis Varese',
-                'punteggio_casa' => 58,
-                'punteggio_ospite' => 67
-            ],
+                [ 
+                    'squadra_casa' => 'Germani Brescia',
+                    'squadra_ospite' => 'Openjobmetis Varese',
+                    'punteggio_casa' => 58,
+                    'punteggio_ospite' => 67
+                ],
 
-            [ 
-                'squadra_casa' => 'Banco di Sardegna Sassari',
-                'squadra_ospite' => 'Fortitudo Bologna',
-                'punteggio_casa' => 52,
-                'punteggio_ospite' => 79
-            ],
+                [ 
+                    'squadra_casa' => 'Banco di Sardegna Sassari',
+                    'squadra_ospite' => 'Fortitudo Bologna',
+                    'punteggio_casa' => 52,
+                    'punteggio_ospite' => 79
+                ],
 
-        ]   
-    ]
+            ]
 
 ?>
 
@@ -79,9 +77,13 @@ $giornata2 = [
 <body>
     <div>
         <ul>
-            <?php for ($i, $i < count($giornata2['']['partita'], $i++) { ?>
+            <?php for ($i = 0; $i < count($giornata2); $i++) { ?>
                 <li>
-                    <?php echo $gioranta2['partita'][$i]]['squadre']; ?>
+                    <!-- <?php var_dump($giornata2);
+                        var_dump($i);
+                        ?> -->
+                    <?php echo $giornata2[$i]['squadra_casa'] . " - " . $giornata2[$i]['squadra_ospite'] . " | " . $giornata2[$i]['punteggio_casa'] . "-" . $giornata2[$i]['punteggio_ospite']; ?>
+                     
                 </li>
             <?php } ?>
 
